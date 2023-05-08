b018ce289d9b072d82750de64a83c78acab97e6c
nasii05 <iamnasih@gmail.com>
2023-05-08 12:57:12 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portrait-gradient';
+
+  @ViewChild('target', {static: true}) targetElement!: ElementRef;
+
+  scrollToTarget(){
+    const targetElement = this.targetElement.nativeElement;
+    targetElement.scrollIntoView({ behavior: 'smooth'});
+  }
 }
